d66ba895be0fa973f16da153d38d0369d972a52f
Corfaya <lauracorrado0696@gmail.com>
2024-09-10 09:47:42 +0200

    [UPD] Bonus line-through on done tasks

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 </div>
                 <div class="col-12">
                     <ul>
-                        <li v-for="(l, i) in todoArray" :key="i"> {{l.name}}</li>
+                        <li v-for="(l, i) in todoArray" :key="i" :class="{'text-decoration-line-through': l.done}"> {{l.name}}</li>
                     </ul>
                 </div>
                 <div class="col-6">
